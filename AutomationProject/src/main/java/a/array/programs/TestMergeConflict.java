84d112afac759f2ad105c940c605f051535f79eb
pradeephbtu@gmail.com <pradeephbtu@gmail.com>
2025-06-28 16:17:57 +0530

    commit in master after branch creted only

---
+++ AutomationProject/src/main/java/a/array/programs/TestMergeConflict.java
@@ -0,0 +1,22 @@
+package a.array.programs;
+
+public class TestMergeConflict {
+	
+	public int getSum(int a,int b) {
+		
+		return a+b;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		TestMergeConflict test=new TestMergeConflict();
+		
+		int c=test.getSum(10, 20);
+		System.out.println(c);
+		
+		
+	}
+	
+
+}
