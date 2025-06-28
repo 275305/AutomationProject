b393e24e17f60ddb981a60d0a28b0ff0fc280659
pradeephbtu@gmail.com <pradeephbtu@gmail.com>
2025-06-28 17:00:49 +0530

    added the getMultiplication method

--- AutomationProject/src/main/java/a/array/programs/TestMergeConflict.java
+++ AutomationProject/src/main/java/a/array/programs/TestMergeConflict.java
@@ -7,13 +7,18 @@ public class TestMergeConflict {
 		return a+b;
 	}
 	
-	
+	public int getSum(int a,int b,int d) {
+		
+		return a*b*d;
+	}
 	public static void main(String[] args) {
 		
 		TestMergeConflict test=new TestMergeConflict();
 		
 		int c=test.getSum(10, 20);
+		
 		System.out.println(c);
+		System.out.println(test.getSum(10, 20,2));
 		
 		
 	}
